291d9162b66c34bd4c130a4363977f9a85ad4f24
Shivansh SIngh <wiseeldrich2004@gmail.com>
2023-09-09 01:09:33 +0530

    comments adding

--- cp/SST CPclub/Week-1/Day4/MarvoloGauntsRing.cpp
+++ cp/SST CPclub/Week-1/Day4/MarvoloGauntsRing.cpp
@@ -13,19 +13,19 @@ int main() {
 
     vector<long long> a(n);
     for (int i = 0; i < n; i++) 
-        cin >> a[i];
+        cin >> a[i];// input array
 
-    vector<long long> ai(n);
-    vector<long long> ak(n);
+    vector<long long> ai(n); // maximum p*ai from left
+    vector<long long> ak(n); // maximum r*ak from right 
 
     ai[0]=a[0]*p;
     ak[n-1]=a[n-1]*r;
-    if(n==1)
+    if(n==1) 
     {
         cout<<a[0]*(p+q+r)<<endl;
         return 0;
     }
-    for (int i = 1; i < n-1; i++) 
+    for (int i = 1; i < n-1; i++) // calculating ai and ak
     {
         ai[i]=max(ai[i-1],a[i]*p);
         ak[n-i-1]=max(ak[n-i],a[n-i-1]*r);
@@ -35,13 +35,32 @@ int main() {
 
     long long int max = LLONG_MIN;
 
-    for (int i = 0; i < n; i++) 
+    for (int j = 0; j < n; j++) // calculating max iteratively over every j 
     {
-        long long int x = ai[i] + a[i] * q + ak[i];
+        long long int x = ai[j] + a[j]*q + ak[j];
         if (x > max) 
             max = x;
     }
     cout << max << endl;
     return 0;
 }
-
+/*
+for(int i=0;i<n;i++)
+{
+    for(int j=i;j<n;j++)
+    {
+        for(int k=j;k<n;k++)
+        {
+            long long int x = a[i]*p + a[j]*q + a[k]*r;
+            if(x>max)
+                max=x;
+        }
+    }
+}// O(n^3) solution tle confirmed
+*/
+/*
+6 1 -1 2
+-1 2 3 -4 5 -6
+-1 2 3 3 5 5
+10 10 10 10 10 -12
+*/
