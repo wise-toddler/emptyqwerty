82670213329567a1b2d3952bf06d5f8ee6a956cc
Shivansh SIngh <wiseeldrich2004@gmail.com>
2023-09-09 03:31:49 +0530

    nhi hua

---
+++ cp/SST CPclub/Week-2/contest/c1/Ultimate_Fencing.cpp
@@ -0,0 +1,41 @@
+// https://www.codechef.com/problems/KEEPOUT 
+#include <iostream>
+#include <vector>
+using namespace std;
+#define int long long
+#define pb push_back
+signed main() {
+	// your code goes here
+	int t;cin >> t;
+	while(t--)
+	{
+	    int n,m;cin >>n>>m;
+	   // vector<int>  v;
+	   // v.pb(0);
+	   // v.pb(m);
+	   
+	   
+	   int max=m-0;
+	   
+	    
+	    for(int i=0;i<n;i++)
+        {
+            int q;cin >>q;
+            v.pb(q);
+        }
+        
+        // sort(v.begin(),v.end())
+	    
+	    // int max=v[1]-v[0];
+	    
+	    // for(int i =1;i<n-1;i++)
+	    // {
+	    //     int j = v[i+1]-v[i];
+	    //     if(j > max)
+	    // }
+	    
+	   
+	}
+	return 0;
+}
+//ask dhruv Sir
