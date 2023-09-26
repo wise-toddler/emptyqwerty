c86baa8c2cd25fd6692ecc7238e071e9711dadaa
Shivansh SIngh <wiseeldrich2004@gmail.com>
2023-09-26 09:33:35 +0530

    sir class banaye ham 2 pointer se karliye

--- cp/SST CPclub/Week-4/problems/G_Coprime_Segment.cpp
+++ cp/SST CPclub/Week-4/problems/G_Coprime_Segment.cpp
@@ -48,9 +48,157 @@ signed main()
             }
             left = rtol;
             ans = min(ans,i-left+1);
+            i=left+1; // sir bass itna hi change kara hogya 
             g=a[i];
         }
     }
     cout << ans << endl;
+    // stack<int> s1,s2;
+    // stack<int> g1,g2;
+    // int ans = n;
+    // // int gc = a[0];
+    // for(int i=0;i<n;i++)
+    // {
+    //     if(!g2.empty() and g2.top()==1)
+    //     {
+    //         ans = min(ans,(int)s1.size()+1);
+    //         g1.push(s2.top());
+    //         while(!s2.empty())
+    //         {
+    //             if(!g1.empty())
+    //                 g1.push(gcd(g1.top(),s2.top()));
+    //             else
+    //                 g1.push(s2.top());
+    //             s2.pop();
+    //             g2.pop();
+    //             if(g1.top()==1)
+    //                 ans = min(ans,(int)g1.size());                
+    //         }
+
+    //     }
+    //     else
+    //     {
+    //         if(!g2.empty())
+    //             g2.push(gcd(g2.top(),a[i]));
+    //         else
+    //             g2.push(a[i]);
+    //         s2.push(a[i]);
+    //     }
+    // }
+    // cout << ans << endl;
 }
-// 
+// 4 6 9 3 6
+// s1:s2
+/*
+    :4              :4
+    :4 6            :4 2
+    :4 6 9          :4 2 1
+        4 6 9:      1 3 9:
+
+*/
+
+
+
+// sir's appraoch
+/*
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define pb push_back
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+
+int gcd(int a, int b) {
+    return a % b == 0 ? b : gcd(b, a % b);
+}
+
+class Queue
+{
+    stack<pii> s1, s2;
+
+    void moveElementsToFirstStack() {
+        while(s2.size()) {
+            int x = s2.top().first;
+            int newGcd = (s1.size()) ? gcd(x, s1.top().second) : x;
+            s1.push({x, newGcd});
+            s2.pop();
+        }
+    }
+
+public:
+
+    void push(int x)  {
+        int newGcd = (s2.size()) ? gcd(x, s2.top().second) : x;
+        s2.push({x, newGcd});
+    }
+
+    void pop() {
+        if(s1.size() == 0) {
+            moveElementsToFirstStack();
+        }
+        s1.pop();
+    }
+
+    bool good() {
+        int g = 0;
+        if(s1.size()) g = gcd(g, s1.top().second);
+        if(s2.size()) g = gcd(g, s2.top().second);
+        return g == 1;
+    }
+
+    bool canRemoveFront() {
+        if(s1.size() == 0) {
+            moveElementsToFirstStack();
+        }
+
+        auto temp = s1.top();
+        s1.pop();
+
+        bool ans = good();
+
+        s1.push(temp);
+
+        return ans;
+    }
+
+    int size() {
+        return s1.size() + s2.size();
+    }
+};
+
+
+signed main() {
+    IOS;
+
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+
+    int ans = INT_MAX;
+    Queue q;
+    for(int x : a) {
+        q.push(x);
+        while(q.size()) {
+            if(q.canRemoveFront()) {
+                q.pop();
+            } else {
+                break;
+            }
+        }
+        if(q.good()) {
+            ans = min(ans, q.size());
+        }
+    }
+
+    if(ans == INT_MAX) ans = -1;
+
+    cout << ans << endl;
+}
+*/
